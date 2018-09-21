f337dfcba5122aeefc1610ce9207ea4427d2efbf
barringtonbear <blakebellis@gmail.com>
2018-09-20 19:19:47 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -30,9 +30,7 @@ function shuffle(array) {
     return array;
 }
 
-var deck = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor",
-           "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf",
-           "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
+
 
 
 
