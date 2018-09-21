11b16cd70ce5b1a28374b7d784c13cfa5d25964f
barringtonbear <blakebellis@gmail.com>
2018-09-20 19:11:24 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -34,13 +34,6 @@ var deck = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", 
            "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf",
            "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
 
-function shuffleCards() {
-    const cardsArray = Array.from(document.querySelectorAll('.deck li'));
-    const cardsAny = shuffle(cardsArray);
-    for (card of cardsAny) {
-        deck.appendChild(card);
-    };
-}
 
 
 function flipCards() {
